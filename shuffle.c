b534eb7d18ae2f430c3e881631dd1f0257873402
Unee0x <Unee0x@gmail.com>
2024-01-19 00:41:47 -0500

    ----

--- shuffle.c
+++ shuffle.c
@@ -4,49 +4,69 @@
 
 #define MAXLEN 10
 
+#define MIN 0
+#define MAX 8
+
 
 int getrand(int min, int max){
   int r = rand() % max + min;
   return r;
 }
 
-int *newarray();
-
 void *mkarray(){
   int *array = malloc(sizeof(int) * MAXLEN);
+  
   array[MAXLEN-1] = '\0';
   if(!array)
     perror("Empty Array");
 
-  int i = 0;
+  static int i = 0;
   int j = 0;
-  while(i < MAXLEN-1){
-    array[j] = j++;
-    i++;
+  while(j < MAXLEN-1){
+    array[j] = ++i;
+    j++;
   }
-
   return array;
 }
 
 void printarray( int *array ){
   int i = 0;
   int *tmp = array;
-  while(*tmp != '\0'){
+  while(i < MAXLEN-1){
     printf("At index %d is a %d\n", i, tmp[i]);
-    tmp++;
     i++;
   }
 }
 
+void swap( int *a, int *b ){
+  int *t = a;
+  a = b;
+  b = t;
+}
+
+void shuffle( int *array ){
+  int count = 0;
+  int buf[] = {'\0'};
+  int r = getrand(MIN, MAX);
+
+  printf("Array[%d] is %d\n", r, array[r]);
+  while(count < MAXLEN-1 && count != r){
+    printf("Count is now %d\n", count);
+    
+    count++;
+  }
+}
+
 int main(){
+  
   srand(time(NULL));
 
   int *array = mkarray();
-
-  //  int n = getrand(0, MAXLEN-1);
-
+  
   printarray(array);
 
+  shuffle(array);
+  
   free(array);
   
   return 0;
